7ab4576200ebd9f584b0fd895873a0c830b1884c
Ny Hasina Vagno <hasina.vagno@esiroi.re>
2026-01-06 01:42:57 +0400

    feat: display geodataframe relevant info

--- main.py
+++ main.py
@@ -7,38 +7,58 @@ from map_utils import show_map
 st.set_page_config(page_title="Geoviz", layout="wide")
 
 st.markdown("""
-    <style>
-    .block-container {
-        padding-top: 0rem;
-        padding-bottom: 0rem;
-        padding-left: 0rem;
-        padding-right: 0rem;
-    }
-    footer {visibility: hidden;}
-    </style>
-    
-    """, unsafe_allow_html=True)
+<style>
+.block-container {
+    padding: 0rem;
+}
+footer {visibility: hidden;}
+</style>
+""", unsafe_allow_html=True)
 
 sidebar = st.sidebar
+uploaded_file = sidebar.file_uploader("", type=["geojson", "json"])
 
-uploaded_file = sidebar.file_uploader("", type=['geojson', 'json'], width=500)
-
-m = folium.Map(zoom_start=20, control_scale=True, tiles='OpenStreetMap')
+m = folium.Map(zoom_start=20, control_scale=True, tiles="OpenStreetMap")
 
 if uploaded_file is not None:
     gdf = gpd.read_file(uploaded_file)
-    show_polygons = sidebar.checkbox("Show polygons", value=False)
 
     if gdf.crs != "EPSG:4326":
         gdf = gdf.to_crs("EPSG:4326")
 
-    if show_polygons:
-        sidebar.write(gdf.head(100))
+    with sidebar.expander("General information", expanded=True):
+        st.write(f"**Features:** {len(gdf)}")
+        st.write(f"**Columns:** {len(gdf.columns)}")
+        st.write(f"**CRS:** {gdf.crs}")
+        st.write(f"**Geometry type(s):** {gdf.geometry.geom_type.unique()}")
 
-    folium.GeoJson(gdf, name="JSON Features").add_to(m)
+    with sidebar.expander("Bounding Box"):
+        bounds = gdf.total_bounds
+        st.write("**(minx, miny, maxx, maxy)**")
+        st.code(bounds)
 
-    bounds = gdf.total_bounds  # [minx, miny, maxx, maxy]
+    with sidebar.expander("Data types"):
+        st.dataframe(
+            gdf.dtypes.reset_index()
+            .rename(columns={"index": "Column", 0: "Type"})
+        )
+
+    with sidebar.expander("Statistics"):
+        st.write(gdf.describe())
+
+    with sidebar.expander("GeoDataFrame"):
+        st.write(gdf)
+
+    folium.GeoJson(
+        gdf,
+        name="GeoJSON",
+        tooltip=folium.GeoJsonTooltip(
+            fields=[c for c in gdf.columns if c != "geometry"]
+        )
+    ).add_to(m)
+
     m.fit_bounds([[bounds[1], bounds[0]], [bounds[3], bounds[2]]])
     show_map(m)
+
 else:
     show_map(m)
